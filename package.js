923f63e81cbd7fdd928e72d830538879da1f338e
Abigail Watson <awatson1978@gmail.com>
2016-02-01 11:47:34 -0800

    drop users before initializing them

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Extended API for Session and Collection.",
-  version: "2.2.2",
+  version: "2.2.3",
   name: "clinical:extended-api",
   git: "https://github.com/clinical-meteor/clinical-extended-api"
 });

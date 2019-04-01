1c341ea162e701221b27b42e6209b6ce31d9d01d
Abigail Watson <awatson1978@gmail.com>
2019-04-01 17:24:09 -0500

    quarterly release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Extended API for Session and Collection.",
-  version: "2.4.0",
+  version: "2.4.1",
   name: "clinical:extended-api",
   git: "https://github.com/clinical-meteor/clinical-extended-api"
 });
